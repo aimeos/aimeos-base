c4f6006f1463abbaf798f45003eae4253aa774f2
Victor Kislichenko <v.kislichenko@gmail.com>
2023-10-07 10:33:08 +0300

    [BUG] php 8.2 object type casting to int (#2)

--- src/Criteria/Expression/Traits.php
+++ src/Criteria/Expression/Traits.php
@@ -223,7 +223,7 @@ trait Traits
 			case 'int':
 			case 'integer':
 			case \Aimeos\Base\DB\Statement\Base::PARAM_INT:
-				$value = $value !== '' ? (int) $value : 'null'; break;
+				$value = $value !== '' ? (int) (string) $value : 'null'; break; // objects must be casted to strings first
 			case 'float':
 			case \Aimeos\Base\DB\Statement\Base::PARAM_FLOAT:
 				$value = $value !== '' ? (double) $value : 'null'; break;
