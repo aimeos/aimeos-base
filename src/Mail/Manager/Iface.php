bf7ae7ecd23b199cf0eceb93cc96a6ca55ef77a1
Aimeos <aimeos@aimeos.org>
2024-06-08 11:29:59 +0200

    Added mail manager interface

---
+++ src/Mail/Manager/Iface.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @license LGPLv3, https://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2024
+ * @package Base
+ * @subpackage Mail
+ */
+
+
+namespace Aimeos\Base\Mail\Manager;
+
+
+/**
+ * Interface for mailer managers
+ *
+ * @package Base
+ * @subpackage Mail
+ */
+interface Iface
+{
+	/**
+	 * Returns the mailer for the given name
+	 *
+	 * @param string $name Key for the mailer
+	 * @return \Aimeos\Base\Mail\Iface Mail object
+	 * @throws \Aimeos\Base\Mail\Exception If an error occurs
+	 */
+	public function get( string $name ) : \Aimeos\Base\Mail\Iface;
+}
