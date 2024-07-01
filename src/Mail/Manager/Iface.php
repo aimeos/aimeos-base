e060418c9cf031e09b48c5f2eaca01d293fd5999
Aimeos <aimeos@aimeos.org>
2024-07-01 10:15:36 +0200

    Make mailer name optional

--- src/Mail/Manager/Iface.php
+++ src/Mail/Manager/Iface.php
@@ -22,9 +22,9 @@ interface Iface
 	/**
 	 * Returns the mailer for the given name
 	 *
-	 * @param string $name Key for the mailer
+	 * @param string|null $name Key for the mailer
 	 * @return \Aimeos\Base\Mail\Iface Mail object
 	 * @throws \Aimeos\Base\Mail\Exception If an error occurs
 	 */
-	public function get( string $name ) : \Aimeos\Base\Mail\Iface;
+	public function get( string $name = null ) : \Aimeos\Base\Mail\Iface;
 }
