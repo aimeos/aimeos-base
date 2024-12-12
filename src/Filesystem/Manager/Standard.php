e229af3217ed581efd23d9e117ddc726ddb08b69
Aimeos <aimeos@aimeos.org>
2024-12-12 13:52:56 +0100

    Fixed return value for __sleep() methods

--- src/Filesystem/Manager/Standard.php
+++ src/Filesystem/Manager/Standard.php
@@ -47,13 +47,15 @@ class Standard implements Iface
 
 	/**
 	 * Clean up the objects inside
+	 *
+	 * @return array List of properties to serialize
 	 */
-	public function __sleep()
+	public function __sleep() : array
 	{
 		$this->__destruct();
 		$this->objects = [];
 
-		return get_object_vars( $this );
+		return array_keys( get_object_vars( $this ) );
 	}
 
 
